9522be72ba5f78b4c224d4484c765d7821729470
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-06-23 23:19:16 +0900

    첫번째 커밋

---
+++ src/main/java/com/example/projectharpseal/ProjectHarpSealApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectharpseal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHarpSealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHarpSealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectharpseal/ProjectHarpSealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectharpseal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHarpSealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
