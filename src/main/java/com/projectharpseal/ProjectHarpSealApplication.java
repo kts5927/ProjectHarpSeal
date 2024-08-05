87fdfb7c1081d3b80e7b1a0733548b770342b75e
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-08-06 03:05:26 +0900

    API 호출 및 환경변수 설정중

--- src/main/java/com/projectharpseal/ProjectHarpSealApplication.java
+++ src/main/java/com/projectharpseal/ProjectHarpSealApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectHarpSealApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProjectHarpSealApplication.class, args);
 	}
 }
