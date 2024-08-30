4bc2789cdd5d4241ad58baaeefea0f072aad8e94
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-08-31 02:12:40 +0900

    aws 프론트 배포 완료5

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,18 +1,10 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
 
 function App() {
-    const [hello, setHello] = useState('')
 
-    useEffect(() => {
-        axios.get('/api/hello')
-            .then(response => setHello(response.data))
-            .catch(error => console.log(error))
-    }, []);
 
     return (
         <div>
-            AWS S3 배포 완료!!!!
+            AWS S3 배포 완료!!!!!
         </div>
     );
 }
