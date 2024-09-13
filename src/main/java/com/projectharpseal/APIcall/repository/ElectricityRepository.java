bd26f9adb02e66b156e2df8e4a668094ac848628
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-09-13 21:38:10 +0900

    DB 커넥션 풀 생성 초기화

--- src/main/java/com/projectharpseal/APIcall/repository/ElectricityRepository.java
+++ src/main/java/com/projectharpseal/APIcall/repository/ElectricityRepository.java
@@ -2,7 +2,6 @@ package com.projectharpseal.APIcall.repository;
 
 
 import com.projectharpseal.APIcall.Entity.Electricity;
-import com.projectharpseal.APIcall.Entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
